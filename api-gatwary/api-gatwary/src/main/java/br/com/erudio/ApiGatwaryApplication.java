f515a8efcec0474e11cf91425a8c278459d80b49
WilliamAraujo237 <william237.araujo@gmail.com>
2023-09-12 17:00:14 -0300

    Primeiro commit do projeto

---
+++ api-gatwary/api-gatwary/src/main/java/br/com/erudio/ApiGatwaryApplication.java
@@ -0,0 +1,13 @@
+package br.com.erudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatwaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatwaryApplication.class, args);
+	}
+
+}
