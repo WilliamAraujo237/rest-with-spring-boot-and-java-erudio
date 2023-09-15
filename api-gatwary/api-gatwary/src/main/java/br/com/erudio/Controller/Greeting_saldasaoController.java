06ba21a2400e08605eae229e98a2515e2eab9ad2
WilliamAraujo237 <william237.araujo@gmail.com>
2023-09-15 14:48:19 -0300

    Adicionando as operacoes de Subtracao,Multiplicacao,Divisao,media e raiz quadrada

--- api-gatwary/api-gatwary/src/main/java/br/com/erudio/Controller/Greeting_saldasaoController.java
+++ api-gatwary/api-gatwary/src/main/java/br/com/erudio/Controller/Greeting_saldasaoController.java
@@ -39,4 +39,57 @@ public class Greeting_saldasaoController {
         String number = strNumber.replaceAll(",",".");
         return number.matches("[-+]?[0-9]*\\.?[0-9]+");
     }
+
+
+    //Break point para requisicao do tipo GET para o envio de dois valores para serem subtraidos
+    @RequestMapping(value = "/sub/{numberOne}/{numberTWO}", method = RequestMethod.GET)
+    public Double sub(@PathVariable("numberOne") String numberOne, @PathVariable("numberTWO") String numberTWO) throws Exception{
+
+        if (!isNumeric(numberOne) || !isNumeric(numberTWO)){
+            throw new UnsuportedMathOperationException("Please inform a Numeric Value !");
+        }
+        return covertToDouble(numberOne) - covertToDouble(numberTWO);
+    }
+
+    //Break point para requisicao do tipo GET para o envio de dois valores para serem multiplicados
+    @RequestMapping(value = "/mult/{numberOne}/{numberTWO}", method = RequestMethod.GET)
+    public Double mult(@PathVariable("numberOne") String numberOne, @PathVariable("numberTWO") String numberTWO) throws Exception{
+
+        if (!isNumeric(numberOne) || !isNumeric(numberTWO)){
+            throw new UnsuportedMathOperationException("Please inform a Numeric Value !");
+        }
+        return covertToDouble(numberOne) * covertToDouble(numberTWO);
+    }
+
+    //Break point para requisicao do tipo GET para o envio de dois valores para serem divididos
+    @RequestMapping(value = "/div/{numberOne}/{numberTWO}", method = RequestMethod.GET)
+    public Double div(@PathVariable("numberOne") String numberOne, @PathVariable("numberTWO") String numberTWO) throws Exception{
+
+        if (!isNumeric(numberOne) || !isNumeric(numberTWO)){
+            throw new UnsuportedMathOperationException("Please inform a Numeric Value !");
+        }
+        return covertToDouble(numberOne) / covertToDouble(numberTWO);
+    }
+
+    //Break point para requisicao do tipo GET para o envio de dois valores para a obtencao da média entre eles
+    @RequestMapping(value = "/med/{numberOne}/{numberTWO}", method = RequestMethod.GET)
+    public Double med(@PathVariable("numberOne") String numberOne, @PathVariable("numberTWO") String numberTWO) throws Exception{
+
+        if (!isNumeric(numberOne) || !isNumeric(numberTWO)){
+            throw new UnsuportedMathOperationException("Please inform a Numeric Value !");
+        }
+        Double soma =  covertToDouble(numberOne) + covertToDouble(numberTWO);
+        return soma / 2;
+    }
+
+    //Break point para requisicao do tipo GET para o envio de dois valores para a raiz quadrada
+    @RequestMapping(value = "/sqrt/{numberOne}/{numberTWO}", method = RequestMethod.GET)
+    public Double sqrt(@PathVariable("numberOne") String numberOne, @PathVariable("numberTWO") String numberTWO) throws Exception{
+
+        if (!isNumeric(numberOne) || !isNumeric(numberTWO)){
+            throw new UnsuportedMathOperationException("Please inform a Numeric Value !");
+        }
+        Double soma =  covertToDouble(numberOne) + covertToDouble(numberTWO);
+        return (Double) Math.sqrt(soma);
+    }
 }
